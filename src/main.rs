ea9fdc6c3e5fd4549d9c9a0d2e320dd43f27710c
Eero <eeero93@gmail.com>
2025-02-10 22:59:46 +0200

    change result handling

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::path::Path;
 
 fn read_csv<P: AsRef<Path>>(filename: P) -> Result<(), Box<dyn Error>> {
+fn read_csv<P: AsRef<Path>>(filename: P) -> Result<DataFrame, Box<dyn Error>> {
     let file = File::open(filename)?;
     let mut rdr = csv::ReaderBuilder::new()
         .delimiter(b'\t')
@@ -17,7 +18,19 @@ fn read_csv<P: AsRef<Path>>(filename: P) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let fpath = "/home/eekoskin22/code/python/autochambers/flux_calc/fluxObject/data/gas_data/211026.DAT";
-    read_csv(fpath)
+fn main() {
+    let fpath =
+        "/home/eerokos/code/python/autochambers/flux_calc/fluxObject/data/gas_data/240126.DAT";
+
+    let df = match read_csv(fpath) {
+        Ok(res) => Some(res),
+        Err(err) => {
+            println!("Crashed with {}", err);
+            None
+        }
+    };
+    if let Some(df) = &df {
+        let s = df.fsecs.clone();
+        let gas = df.gas.clone();
+    }
 }
