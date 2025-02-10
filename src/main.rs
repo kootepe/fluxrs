7aedd7f28e5e2754dc0d165ed028b006a784d75f
Eero <eeero93@gmail.com>
2025-02-10 23:00:59 +0200

    use df

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,65 @@ fn read_csv<P: AsRef<Path>>(filename: P) -> Result<DataFrame, Box<dyn Error>> {
     }
 
     Ok(())
+    let mut gas: Vec<f64> = Vec::new();
+    let mut diag: Vec<u32> = Vec::new();
+    let mut date: Vec<String> = Vec::new();
+    let mut time: Vec<String> = Vec::new();
+    //let mut ntime: Vec<f64> = Vec::new();
+    let mut fsecs: Vec<f64> = Vec::new();
+    let mut secs: Vec<u64> = Vec::new();
+    let mut nsecs: Vec<u64> = Vec::new();
+    let mut header = csv::StringRecord::new();
+
+    for (i, r) in rdr.records().enumerate() {
+        let record: &csv::StringRecord = &r?;
+
+        if i == 0 {
+            header = record.clone();
+            continue;
+        }
+        if i == 1 {
+            continue;
+        }
+        date.push(record[6].to_string());
+        time.push(record[7].to_string());
+
+        if let Ok(val) = record[9].parse::<f64>() {
+            gas.push(val)
+        }
+        if let Ok(val) = record[4].parse::<u32>() {
+            diag.push(val)
+        }
+
+        if let Ok(val) = record[1].parse::<u64>() {
+            secs.push(val)
+        }
+        if let Ok(val) = record[2].parse::<u64>() {
+            nsecs.push(val)
+        }
+        if let Ok(val) = record[2].parse::<f64>() {
+            fsecs.push(val)
+        }
+    }
+
+    let datetime: Vec<DateTime<Utc>> = secs
+        .iter()
+        .zip(nsecs.iter())
+        .map(|(&sec, &nsec)| {
+            let d = UNIX_EPOCH + Duration::from_secs(sec) + Duration::from_nanos(nsec);
+            DateTime::<Utc>::from(d) // Convert to DateTime<Utc>
+        })
+        .collect();
+    let df = DataFrame {
+        header,
+        datetime,
+        secs,
+        fsecs,
+        nsecs,
+        gas,
+        diag,
+    };
+    Ok(df)
 }
 
 fn main() {
