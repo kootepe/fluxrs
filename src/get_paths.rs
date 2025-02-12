eb9145fd0c8afa871a761165ca87aab603f85470
Eero Koskinen <Eero Koskinen>
2025-02-12 11:31:53 +0200

    cleanup

--- src/get_paths.rs
+++ src/get_paths.rs
@@ -35,35 +35,3 @@ pub fn get_paths(parg: String, string: &str) -> Result<Vec<PathBuf>, Box<dyn Err
     };
     Ok(paths)
 }
-
-// pub fn get_time_paths(parg: &str) -> Result<Vec<PathBuf>, Box<dyn Error>> {
-//     let paths: Vec<PathBuf> = if !parg.is_empty() {
-//         glob(parg)
-//             .expect("Failed to read glob pattern")
-//             .filter_map(Result::ok)
-//             .collect()
-//     } else {
-//         println!("Give path pattern to time data (e.g., *.txt):");
-//         loop {
-//             let mut input = String::new();
-//             io::stdin()
-//                 .read_line(&mut input)
-//                 .expect("Failed to read line");
-//
-//             let value = input.trim().to_string();
-//             println!("Pattern entered: {}", value);
-//
-//             let paths: Vec<PathBuf> = glob(&value)
-//                 .expect("Failed to read glob pattern")
-//                 .filter_map(Result::ok)
-//                 .collect();
-//
-//             if !paths.is_empty() {
-//                 break paths;
-//             } else {
-//                 println!("No files matched the pattern. Try again.");
-//             }
-//         }
-//     };
-//     Ok(paths)
-// }
