8255bec60a5f7852c3ba5622b996a1739593d51d
Eero Koskinen <Eero Koskinen>
2025-02-12 11:37:17 +0200

    Add error for empty list
    There was no proper error when passing args with the command

--- src/get_paths.rs
+++ src/get_paths.rs
@@ -6,10 +6,14 @@ use std::path::PathBuf;
 // pub fn get_paths(args: Vec<String>) -> Result<Vec<PathBuf>, Box<dyn Error>> {
 pub fn get_paths(parg: String, string: &str) -> Result<Vec<PathBuf>, Box<dyn Error>> {
     let paths: Vec<PathBuf> = if !parg.is_empty() {
-        glob(&parg)
+        let vec: Vec<_> = glob(&parg)
             .expect("Failed to read glob pattern")
             .filter_map(Result::ok)
-            .collect()
+            .collect();
+        if vec.is_empty() {
+            eprintln!("No {string} files found with {parg}");
+        }
+        vec
     } else {
         println!("Give path pattern to {string} data (e.g., *.txt or 24*.DAT):");
         loop {
