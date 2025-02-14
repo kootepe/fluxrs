df15759779eec0109b2206fffadd62cf0a2957e0
Eero <eeero93@gmail.com>
2025-02-14 12:59:05 +0200

    Clarity

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::process;
 use fluxrs::Config;
 
 fn main() {
-    let args = env::args();
-    let config = Config::build(args).unwrap_or_else(|err| {
+    let inputs = env::args();
+    let config = Config::build(inputs).unwrap_or_else(|err| {
         println!("Parsing problem {err}");
         process::exit(1)
     });
