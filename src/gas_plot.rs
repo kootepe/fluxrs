02f0d60044592baeebcdec3bff23fced835ec426
Eero <eeero93@gmail.com>
2025-02-16 22:46:21 +0200

    cleanup/black plot

--- src/gas_plot.rs
+++ src/gas_plot.rs
@@ -3,7 +3,7 @@ use plotters::element::DashedPathElement;
 use plotters::prelude::*;
 use std::error::Error;
 
-pub const PLOT_HEIGHT: u32 = 50;
+pub const PLOT_HEIGHT: u32 = 40;
 pub const PLOT_WIDTH: u32 = PLOT_HEIGHT * 2;
 
 pub fn draw_gas_plot(cycle: &Cycle) -> Result<String, Box<dyn Error>> {
@@ -24,10 +24,8 @@ pub fn draw_gas_plot(cycle: &Cycle) -> Result<String, Box<dyn Error>> {
     let xmax = time_data.last().unwrap();
     let cxmin = calc_time_data[0];
     let cxmax = calc_time_data.last().unwrap();
-    let mut closet = Vec::new();
+
     let gas_data = &cycle.gas_v;
-    // let ymin = *gas_data.iter().min_by(|a, b| a.total_cmp(b)).unwrap();
-    // let ymax = *gas_data.iter().max_by(|a, b| a.total_cmp(b)).unwrap();
     let ymin = cycle
         .gas_v
         .iter()
@@ -38,46 +36,64 @@ pub fn draw_gas_plot(cycle: &Cycle) -> Result<String, Box<dyn Error>> {
         .gas_v
         .iter()
         .cloned()
+        .rev()
+        .take(120)
         .filter(|v| !v.is_nan())
         .fold(f64::NEG_INFINITY, f64::max);
 
-    let datetime_idx = cycle.get_peak_datetime().unwrap().timestamp() as f64;
-    closet.push((datetime_idx, ymax));
-    closet.push((datetime_idx, ymin));
-    let rect = [(*cxmax, ymax), (cxmin, ymin)];
+    let close_line = vec![
+        (cycle.close_time.timestamp() as f64, 10000000.),
+        (cycle.close_time.timestamp() as f64, 0.),
+    ];
+    // let open_line = vec![
+    //     (cycle.open_time.timestamp() as f64, 10000000.),
+    //     (cycle.open_time.timestamp() as f64, 0.),
+    // ];
+    let max_line = vec![(cycle.max_idx, 10_000_000.), (cycle.max_idx, 0.)];
+    let rect = [(*cxmax, 10_000_000.), (cxmin, -10_000_000.)];
     let data: Vec<(f64, f64)> = time_data
         .iter()
         .zip(gas_data.iter())
         .map(|(&t, &g)| (t, g))
         .collect();
-    let style = ShapeStyle {
-        color: RED.mix(0.3),
+    let rect_style = ShapeStyle {
+        color: WHITE.mix(0.30),
         filled: true,
         stroke_width: 5,
     };
+    let bg_col = RGBColor(10, 11, 10);
     let root_area = BitMapBackend::new(&wpath, (PLOT_WIDTH, PLOT_HEIGHT)).into_drawing_area();
-    root_area.fill(&WHITE).unwrap();
+    root_area.fill(&bg_col).unwrap();
 
-    let xra = xmax - xmin;
-    let yra = ymax - ymin;
+    // let xrange = xmax - xmin;
+    // let yrange = ymax - ymin;
     let mut ctx = ChartBuilder::on(&root_area)
-        // .set_label_area_size(LabelAreaPosition::Left, 50)
-        // .set_label_area_size(LabelAreaPosition::Bottom, 50)
-        // .margin_right(50)
-        // .caption("Scatter Demo", ("sans-serif", 50))
-        // .build_cartesian_2d(-10.0..50.0, -10.0..50.0)
+        // 5% buffer around the data
         .build_cartesian_2d(
-            (xmin - (xra * 0.05))..(*xmax + (xra * 0.05)),
-            (ymin - (yra * 0.05))..(ymax + (yra * 0.05)),
+            (xmin)..(*xmax),
+            (ymin)..(ymax),
+            // (xmin - (xrange * 0.05))..(*xmax + (xrange * 0.05)),
+            // (ymin - (yrange * 0.05))..(ymax + (yrange * 0.05)),
         )
         .unwrap();
 
     ctx.configure_mesh().disable_mesh().draw().unwrap();
 
-    let col = RGBColor(105, 239, 83);
-    ctx.draw_series(data.iter().map(|point| Cross::new(*point, 1, col.mix(0.6))))
-        .unwrap();
-    ctx.draw_series(std::iter::once(DashedPathElement::new(closet, 11, 5, RED)))?;
-    // ctx.draw_series(std::iter::once(Rectangle::new(rect, style)))?;
+    let pt_col = RGBColor(105, 239, 83);
+    ctx.draw_series(
+        data.iter()
+            .map(|point| Cross::new(*point, 1, pt_col.mix(0.6))),
+    )
+    .unwrap();
+    ctx.draw_series(std::iter::once(DashedPathElement::new(
+        max_line, 11, 5, RED,
+    )))?;
+    ctx.draw_series(std::iter::once(DashedPathElement::new(
+        close_line, 11, 5, BLACK,
+    )))?;
+    // ctx.draw_series(std::iter::once(DashedPathElement::new(
+    //     open_line, 11, 5, GREEN,
+    // )))?;
+    ctx.draw_series(std::iter::once(Rectangle::new(rect, rect_style)))?;
     Ok(root.clone())
 }
