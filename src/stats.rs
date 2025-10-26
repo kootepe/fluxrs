1b072c10b17fc86b499c3030542858ad055f087e
Eero <eeero93@gmail.com>
2025-10-26 22:04:21 +0200

    slope formula is value minus the mean

--- src/stats.rs
+++ src/stats.rs
@@ -42,13 +42,13 @@ impl LinReg {
         assert!(x.len() == y.len(), "Input vectors x and y must have the same length");
 
         let avg_x: f64 = x.iter().sum::<f64>() / x.len() as f64;
-        let x_differences_to_average: Vec<f64> = x.iter().map(|value| avg_x - value).collect();
+        let x_differences_to_average: Vec<f64> = x.iter().map(|value| value - avg_x).collect();
         let x_differences_to_average_squared: Vec<f64> =
             x_differences_to_average.iter().map(|value| value.powi(2)).collect();
         let ss_xx: f64 = x_differences_to_average_squared.iter().sum();
 
         let avg_y = y.iter().sum::<f64>() / y.len() as f64;
-        let y_differences_to_average: Vec<f64> = y.iter().map(|value| avg_y - value).collect();
+        let y_differences_to_average: Vec<f64> = y.iter().map(|value| value - avg_x).collect();
         let x_and_y_differences_multiplied: Vec<f64> = x_differences_to_average
             .iter()
             .zip(y_differences_to_average.iter())
