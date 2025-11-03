eb4442dab949a4b974de62c2ebdfdde8d5c0d23a
Eero <eeero93@gmail.com>
2025-11-03 14:43:03 +0200

    add manageapp

--- src/ui/manage_proj/manage_ui.rs
+++ src/ui/manage_proj/manage_ui.rs
@@ -1,27 +1,55 @@
-use crate::instruments::instruments::InstrumentType;
-use crate::ui::manage_proj::project_ui::MsgType;
+use crate::ui::manage_proj::project::Project;
 use crate::ui::manage_proj::project_ui::{clicked_outside_window, ProjectApp};
-use crate::ui::tz_picker::timezone_combo;
-use crate::ui::validation_ui::Mode;
 use egui::{Align2, Area, Color32, Context, Frame, Id, Window};
-use std::error::Error;
 
-impl ProjectApp {
-    fn close_manage_proj(&mut self) {
-        self.proj_manage_open = false;
+pub struct ManageApp {
+    pub open: bool,
+    project: Project,
+    delete_measurement: bool,
+    delete_fluxes: bool,
+    delete_meteo: bool,
+    delete_height: bool,
+    delete_chamber: bool,
+}
+
+impl Default for ManageApp {
+    fn default() -> Self {
+        Self::new()
     }
-    pub fn show_manage_proj_data(&mut self, ctx: &egui::Context) {
-        if !self.proj_manage_open {
+}
+
+impl ManageApp {
+    pub fn new() -> Self {
+        Self {
+            open: false,
+            project: Project::default(),
+            delete_measurement: false,
+            delete_fluxes: false,
+            delete_meteo: false,
+            delete_height: false,
+            delete_chamber: false,
+        }
+    }
+}
+
+impl ManageApp {
+    fn close_manage_proj(&mut self) {
+        self.open = false;
+    }
+    pub fn show_manage_proj_data(&mut self, ctx: &egui::Context, project: Project) {
+        self.project = project;
+
+        if !self.open {
             return;
         }
         if ctx.input(|i| i.key_pressed(egui::Key::Escape)) {
-            self.proj_manage_open = false;
+            self.open = false;
             return;
         }
 
         let can_close = true;
         let wr = Window::new("Manage project")
-            .open(&mut self.proj_manage_open)
+            .open(&mut self.open)
             .title_bar(false)
             .collapsible(false)
             .resizable(false)
@@ -33,11 +61,23 @@ impl ProjectApp {
                     .inner_margin(egui::Margin::symmetric(16, 12)),
             )
             .show(ctx, |ui| {
-                ui.button("Delete measurement data");
-                ui.button("Delete calculated fluxes");
-                ui.button("Delete meteo data");
-                ui.button("Delete height data");
-                ui.button("Delete chamber data");
+                ui.horizontal(|ui| {
+                    if ui.button("Delete measurement data").clicked() {
+                        self.delete_measurement = true;
+                    };
+                    if ui.button("Delete calculated fluxes").clicked() {
+                        self.delete_fluxes = true;
+                    };
+                    if ui.button("Delete meteo data").clicked() {
+                        self.delete_meteo = true;
+                    };
+                    if ui.button("Delete height data").clicked() {
+                        self.delete_height = true;
+                    };
+                    if ui.button("Delete chamber data").clicked() {
+                        self.delete_chamber = true;
+                    };
+                });
             });
 
         if clicked_outside_window(ctx, wr.as_ref()) && can_close {
