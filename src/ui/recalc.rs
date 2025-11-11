4841b862ff80edd12222ff0b7612110a392ea502
Eero <eeero93@gmail.com>
2025-11-12 00:19:27 +0200

    separate calculation and ui

--- src/ui/recalc.rs
+++ src/ui/recalc.rs
@@ -37,40 +37,38 @@ impl RecalculateApp {
         }
     }
 
-    pub fn ui(
-        &mut self,
-        ui: &mut Ui,
+    pub fn calculate(
+        &self,
         runtime: &tokio::runtime::Runtime,
         start_date: DateTime<Utc>,
         end_date: DateTime<Utc>,
         project: Project,
         progress_sender: mpsc::UnboundedSender<ProcessEvent>,
     ) {
-        ui.vertical(|ui| {
-        ui.label("Compare the current chamber height of all calculated fluxes and recalculate if a new one is found.");
-        ui.label("Only changes the fluxes and height, no need to redo manual validation.");
-
-
-        if ui.add_enabled(self.calc_enabled && !self.calc_in_progress,egui::Button::new("Recalculate.")).clicked() {
-                self.calc_enabled = false;
-                self.query_in_progress = true;
-
         let conn = match Connection::open("fluxrs.db") {
             Ok(conn) => conn,
             Err(e) => {
-                let _ = progress_sender.send(ProcessEvent::Query(QueryEvent::DbFail(e.to_string())));
+                let _ =
+                    progress_sender.send(ProcessEvent::Query(QueryEvent::DbFail(e.to_string())));
                 // log_messages.push_front(bad_message(&"Failed to open database."));
                 return;
             },
         };
+        let progsender = progress_sender.clone();
 
         let arc_conn = Arc::new(Mutex::new(conn));
         let proj = project.clone();
 
         // TODO: load_cycles queries chambers internally so they are getting set twice.
         runtime.spawn(async move {
-            let cycle_result =
-                load_cycles(arc_conn.clone(), start_date, end_date, proj.clone(), progress_sender.clone()).await;
+            let cycle_result = load_cycles(
+                arc_conn.clone(),
+                start_date,
+                end_date,
+                proj.clone(),
+                progress_sender.clone(),
+            )
+            .await;
             let meteo_result =
                 query_meteo_async(arc_conn.clone(), start_date, end_date, proj.clone()).await;
             let height_result =
@@ -80,6 +78,8 @@ impl RecalculateApp {
             match (cycle_result, meteo_result, height_result, chamber_result) {
                 (Ok(cycle_data), Ok(meteo_data), Ok(height_data), Ok(chamber_data)) => {
                     let _ = progress_sender.send(ProcessEvent::Query(QueryEvent::QueryComplete));
+                    let _ = progsender
+                        .send(ProcessEvent::Progress(ProgressEvent::Recalced(0, cycle_data.len())));
                     if !cycle_data.is_empty() {
                         let processor = Recalcer::new(
                             project.clone(),
@@ -96,26 +96,14 @@ impl RecalculateApp {
                         let _ = progress_sender.send(ProcessEvent::Done(Err(msg.to_owned())));
                     }
                 },
-                e => {eprintln!("Failed to query database: {:?}", e);
-                        let msg = format!("Failed to query database: {:?}", e);
-            let _ = progress_sender.send(ProcessEvent::Done(Err(msg)));}
+                e => {
+                    eprintln!("Failed to query database: {:?}", e);
+                    let msg = format!("Failed to query database: {:?}", e);
+                    let _ = progress_sender.send(ProcessEvent::Done(Err(msg)));
+                },
             }
-        });}
-    });
-        if self.calc_in_progress || !self.calc_enabled {
-            ui.add(egui::Spinner::new());
-            if self.query_in_progress {
-                ui.label("Querying data, this can take a while for large time ranges.");
-            } else if let Some((_, total)) = self.cycles_state {
-                let pb =
-                    egui::widgets::ProgressBar::new(self.cycles_progress as f32 / total as f32)
-                        .desired_width(200.)
-                        .corner_radius(1)
-                        .show_percentage()
-                        .text(format!("{}/{}", self.cycles_progress, total));
-                ui.add(pb);
-            } else {
-                ui.label("Recalculating fluxes");
+        });
+    }
 
     pub fn ui(
         &mut self,
